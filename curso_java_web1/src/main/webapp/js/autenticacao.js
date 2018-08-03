a90b875e6f4baa2bc53941e1d004c4d7aba03737
TI <TI@TI01>
2018-08-03 17:54:30 -0300

    03/08/18

--- curso_java_web1/src/main/webapp/js/autenticacao.js
+++ curso_java_web1/src/main/webapp/js/autenticacao.js
@@ -1,27 +1,26 @@
 
 //Classe
 Autenticacao = function () {
-				
+    //Método
+    this.validarCampos = function () {
+
+        elementoUsuario = document.getElementById("txtUsuário");
+        textDigitadoUsuario = elementoUsuario.value;
+
+        elementoSenha = document.getElementById("txtSenha");
+        textDigitadoSenha = elementoSenha.value;
+
+        if (textDigitadoUsuario=="" || textDigitadoSenha=="") {
+            window.alert("#Os campos usuário e senha devem ser digitados!#");
+            return false;
+        } else {
+            return true;
+        }
+
+    }		
 }
 
-//Método
-Autenticacao.prototype.validarCampos = function () {
-    elementoUsuario = document.getElementById("txtUsuário");
-    textDigitadoUsuario = elementoUsuario.value;
 
-    elementoSenha = document.getElementById("txtSenha");
-    textDigitadoSenha = elementoSenha.value;
-
-    if (textDigitadoUsuario=="" || textDigitadoSenha=="") {
-        window.alert("#Os campos usuário e senha devem ser digitados!#");
-        return false;
-    } else {
-        return true;
-    }
-
-}
 
 //Instanciamos o objeto
 aut = new Autenticacao();
-
-//Exemplo alterar do git...
